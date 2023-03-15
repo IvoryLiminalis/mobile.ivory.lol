d87d105d329632ebd23642a2442dab614e41381f
Ivory <118472967+IvoryLiminalis@users.noreply.github.com>
2023-03-15 11:23:19 +0200

    Update indexmobile.js

--- js/indexmobile.js
+++ js/indexmobile.js
@@ -15,22 +15,27 @@ function Toggle1() {
   const x2 = document.getElementById('two');
 	const x3 = document.getElementById('three');
 	const x4 = document.getElementById('four');
+	const x5 = document.getElementById('five');
   const btn1 = document.getElementById('button1');
-  if (x.style.opacity === '1' || x3.style.opacity === '1') {
+  if (x.style.opacity === '1' || x3.style.opacity === '1' || x4.style.opacity === '1' || x5.style.opacity === '1') {
     x.style.transitionDelay = '0s';
     x2.style.transitionDelay = '1s';
     x3.style.transitionDelay = '0s';
 		x4.style.transitionDelay = '0s';
+		x5.style.transitionDelay = '0s';
 		x4.style.pointerEvents = "none";
+		x5.style.pointerEvents = "none";
     x.style.opacity = '0';
     x2.style.opacity = '1';
 		x3.style.opacity = '0';
 		x4.style.opacity = '0';
+		x5.style.opacity = '0';
     x.style.zIndex = '0';
     x2.style.zIndex = '1';
     x3.style.zIndex = '0';
     x4.style.zIndex = '0';
-		btn1.style.marginTop = '-2px'
+		x5.style.zIndex = '0';
+		btn1.style.marginTop = '-2px';
 
   } else {
     x.style.transitionDelay = '1s';
@@ -38,15 +43,18 @@ function Toggle1() {
     x3.style.transitionDelay = '0s';
 		x4.style.transitionDelay = '0s';
 		x4.style.pointerEvents = "none";
+		x5.style.pointerEvents = "none";
     x.style.opacity = '1';
     x2.style.opacity = '0';
 		x3.style.opacity = '0';
 		x4.style.opacity = '0';
+		x5.style.opacity = '0';
     x.style.zIndex = '1';
     x2.style.zIndex = '0';
     x3.style.zIndex = '0';
 		x4.style.zIndex = '0';
-		btn1.style.marginTop = '-2px'
+		x5.style.zIndex = '0';
+		btn1.style.marginTop = '-2px';
   }
 }
 
@@ -55,44 +63,53 @@ function ShowPage1() {
   const x2 = document.getElementById('two');
 	const x3 = document.getElementById('three');
 	const x4 = document.getElementById('four');
+	const x5 = document.getElementById('five');
   const btn1 = document.getElementById('button1');
     x.style.transitionDelay = '1s';
     x2.style.transitionDelay = '0s';
     x3.style.transitionDelay = '0s';
-    x4.style.transitionDelay = '0s';
+		x4.style.transitionDelay = '0s';
+		x5.style.transitionDelay = '0s';
 		x4.style.pointerEvents = "none";
+		x5.style.pointerEvents = "none";
     x.style.opacity = '1';
     x2.style.opacity = '0';
 		x3.style.opacity = '0';
 		x4.style.opacity = '0';
+		x5.style.opacity = '0';
     x.style.zIndex = '1';
     x2.style.zIndex = '0';
     x3.style.zIndex = '0';
 		x4.style.zIndex = '0';
-		btn1.style.marginTop = '-2px'
+		x5.style.zIndex = '0';
+		btn1.style.marginTop = '-2px';
 }
-
 
 function ShowPage2() {
   const x = document.getElementById('one-wrap');
   const x2 = document.getElementById('two');
 	const x3 = document.getElementById('three');
 	const x4 = document.getElementById('four');
+	const x5 = document.getElementById('five');
   const btn1 = document.getElementById('button1');
     x.style.transitionDelay = '0s';
     x2.style.transitionDelay = '0s';
     x3.style.transitionDelay = '1s';
-    x4.style.transitionDelay = '0s';
+		x4.style.transitionDelay = '0s';
+		x5.style.transitionDelay = '0s';
 		x4.style.pointerEvents = "none";
+		x5.style.pointerEvents = "none";
     x.style.opacity = '0';
     x2.style.opacity = '0';
-    x3.style.opacity = '1';
+		x3.style.opacity = '1';
 		x4.style.opacity = '0';
+		x5.style.opacity = '0';
     x.style.zIndex = '0';
     x2.style.zIndex = '0';
     x3.style.zIndex = '1';
 		x4.style.zIndex = '0';
-		btn1.style.marginTop = '10px'
+		x5.style.zIndex = '0';
+		btn1.style.marginTop = '-2px';
 }
 
 function ShowPage3() {
@@ -100,19 +117,51 @@ function ShowPage3() {
   const x2 = document.getElementById('two');
 	const x3 = document.getElementById('three');
 	const x4 = document.getElementById('four');
+	const x5 = document.getElementById('five');
   const btn1 = document.getElementById('button1');
     x.style.transitionDelay = '0s';
     x2.style.transitionDelay = '0s';
     x3.style.transitionDelay = '0s';
-    x4.style.transitionDelay = '1s';
-		x4.style.pointerEvents = "auto";
+		x4.style.transitionDelay = '1s';
+		x5.style.transitionDelay = '0s';
+		x4.style.pointerEvents = "initial";
+		x5.style.pointerEvents = "none";
     x.style.opacity = '0';
     x2.style.opacity = '0';
-    x3.style.opacity = '0';
+		x3.style.opacity = '0';
 		x4.style.opacity = '1';
+		x5.style.opacity = '0';
     x.style.zIndex = '0';
     x2.style.zIndex = '0';
     x3.style.zIndex = '0';
 		x4.style.zIndex = '1';
-		btn1.style.marginTop = '10px'
+		x5.style.zIndex = '0';
+		btn1.style.marginTop = '-2px';
+}
+
+function ShowPage4() {
+  const x = document.getElementById('one-wrap');
+  const x2 = document.getElementById('two');
+	const x3 = document.getElementById('three');
+	const x4 = document.getElementById('four');
+	const x5 = document.getElementById('five');
+  const btn1 = document.getElementById('button1');
+    x.style.transitionDelay = '0s';
+    x2.style.transitionDelay = '0s';
+    x3.style.transitionDelay = '0s';
+		x4.style.transitionDelay = '0s';
+		x5.style.transitionDelay = '1s';
+		x4.style.pointerEvents = "none";
+		x5.style.pointerEvents = "initial";
+    x.style.opacity = '0';
+    x2.style.opacity = '0';
+		x3.style.opacity = '0';
+		x4.style.opacity = '0';
+		x5.style.opacity = '1';
+    x.style.zIndex = '0';
+    x2.style.zIndex = '0';
+    x3.style.zIndex = '0';
+		x4.style.zIndex = '0';
+		x5.style.zIndex = '1';
+		btn1.style.marginTop = '-2px';
 }
